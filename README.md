a2d65c57fab0b031d7f2276249c22a14c8b99c33
larisaturcu <turcu.larisa@gmail.com>
2021-05-20 11:53:00 +0200

    refactor index.js to use async/await in task and user controller

--- README.md
+++ README.md
@@ -76,6 +76,7 @@ const doWork = async () => {
   const sum = await add(1, 99)
   const sum2 = await add(sum, 50);
   const sum3 = await add(sum2, 3);
+  return sum3;
 }
 
 
--- src/playground/promise-chaining.js
+++ src/playground/promise-chaining.js
@@ -7,10 +7,24 @@ const User = require('../models/user')
 //     console.log(userUpdated)
 // }));
 
-User.findByIdAndUpdate("609a7f22f7857c2ef495dff2", { "email": "tst@gmail.com" }).then((user) => {
-    console.log(user);
-    return User.countDocuments({"age": 0})
-}).then((result) => {
-    console.log(result);
+// User.findByIdAndUpdate("609a7f22f7857c2ef495dff2", { "email": "tst@gmail.com" }).then((user) => {
+//     console.log(user);
+//     return User.countDocuments({"age": 0})
+// }).then((result) => {
+//     console.log(result);
+// }
+// )
+
+
+
+const updateAgeAndCount = async (userId, age) => {
+    await User.findByIdAndUpdate(userId, { age });
+    const total =  await User.countDocuments({age})
+    return total;
 }
-)
+
+updateAgeAndCount('60a3de2d160dab4b841164e8', 22).then( (result) => {
+    console.log(result);
+}).catch ((e) => {
+    console.log(e);
+})
